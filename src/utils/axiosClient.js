d72a4765df756d41c1a42625b8a47003837771ad
Abhishek Kumar <abhishekkumar04923@gmail.com>
2026-02-23 18:53:12 +0530

    production ready frontend config

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
